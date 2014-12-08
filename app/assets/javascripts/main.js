b398511d6f70a17b6291a3bd31e7db5bd4764afa
Peter Jang <peter.x.jang@gmail.com>
2014-12-08 17:02:49 -0600

    working example for autocomplete from remote data source

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,12 +2,39 @@ $(document).ready(function() {
 
 	$(document).on('click', '#header-menu', function(e) {
 		e.preventDefault();
-		console.log('menu');
 	});
 
 	$(document).on('click', '#header-search', function(e) {
 		e.preventDefault();
-		console.log('search');
 	});
+
+  $( "#autocomplete" ).on( "filterablebeforefilter", function ( e, data ) {
+    var $ul = $( this ),
+      $input = $( data.input ),
+      value = $input.val(),
+      html = "";
+    $ul.html( "" );
+    if ( value && value.length > 2 ) {
+    	console.log(value);
+      $ul.html( "<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>" );
+      $ul.listview( "refresh" );
+      $.ajax({
+        url: "http://gd.geobytes.com/AutoCompleteCity",
+        dataType: "jsonp",
+        crossDomain: true,
+        data: {
+          q: $input.val()
+        }
+      })
+      .then( function ( response ) {
+        $.each( response, function ( i, val ) {
+          html += "<li><a href='#'>" + val + "</a></li>";
+        });
+        $ul.html( html );
+        $ul.listview( "refresh" );
+        $ul.trigger( "updatelayout");
+      });
+    }
+  });
 
 });
