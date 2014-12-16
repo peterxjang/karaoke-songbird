f68da9e40c1f97941ac6269dd9eb3cffcaee3af3
Peter Jang <peter.x.jang@gmail.com>
2014-12-16 09:17:01 -0600

    changed seeds to use actual song database from Pablo's csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,23 @@
+require 'csv'
+
 User.delete_all
 Song.delete_all
 UserSong.delete_all
 Playlist.delete_all
 
 # Create songs
-artists = Array.new(20) { Faker::Name.name }
-100.times do 
-	song = Song.create!(title: Faker::App.name,
-											artist: artists.sample,
-											duration: (120..360).to_a.sample)
+# artists = Array.new(20) { Faker::Name.name }
+# 100.times do 
+# 	song = Song.create!(title: Faker::App.name,
+# 											artist: artists.sample,
+# 											duration: (120..360).to_a.sample)
+# end
+CSV.foreach("karaoke Library.csv") do |row|
+  song = Song.create!(title: row[1],
+  										artist: row[0],
+  										duration: (120..360).to_a.sample)
 end
+
 
 # Create users
 emails = ['admin@gmail.com', 'user@gmail.com'] + Array.new(4) { Faker::Internet.email }
